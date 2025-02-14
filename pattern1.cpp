5be55be01f2be243889182546fba04e2c0e4ecbd
anonymouswizard-spell <ashuujanveja@gmail.com>
2025-02-14 13:06:53 +0530

    second commit

--- pattern1.cpp
+++ pattern1.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 int main()
 {
-    int n;
-    cout<<"enter number of columns";
+    int n;//this is a new comment
+    cout<<"enter number of columns"<<endl;
     cin>>n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
